6ecaf0640e279c2ff2c2cd9eefdbcc8cd2490bac
AlejandroDiaz <alejandro.hernandez@bootcamp-upgrade.com>
2021-01-22 13:02:28 +0100

    Sidebar complete, needs functionality

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,6 +4,8 @@ import {Link} from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import {SidebarData} from './SidebarData';
+import SubMenu from './SubMenu';
+import { IconContext } from 'react-icons/lib';
 
 const Nav = styled.div`
     background: #15171c;
@@ -46,6 +48,7 @@ const Sidebar = () => {
 
     return (
         <>
+        <IconContext.Provider value={{color: 'fff'}}>
             <Nav>
                 <NavIcon to="#" >
                     <FaIcons.FaBars onClick={showSidebar} />
@@ -56,8 +59,12 @@ const Sidebar = () => {
                     <NavIcon to="#" >
                         <AiIcons.AiOutlineClose onClick={showSidebar} />
                     </NavIcon>
+                    {SidebarData.map((item, index) => {
+                        return <SubMenu item={item} key={index} />
+                    })}
                 </SidebarWrap>
             </SidebarNav>
+        </IconContext.Provider>
         </>
     )
 }
